49e33cc9c0903afae16371145538a803b64e984f
AAAstorga <astorgaaustin@gmail.com>
2016-12-24 21:10:10 -0800

    Started work on the signupVC. Along with adding a navBar.

--- FitCat2/SignUpViewController.swift
+++ FitCat2/SignUpViewController.swift
@@ -1,5 +1,5 @@
 //
-//  InitialViewController.swift
+//  SignUpViewController.swift
 //  FitCat2
 //
 //  Created by Austin Astorga on 12/23/16.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class InitialViewController: UIViewController, UITextFieldDelegate {
+class SignUpViewController: UIViewController, UITextFieldDelegate {
     
     
     let gradient = CAGradientLayer()
@@ -24,7 +24,6 @@ class InitialViewController: UIViewController, UITextFieldDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpGradient()
-        self.navigationItem.title = ""
         
         //Button set up
         let buttonWidth = view.frame.width * 0.828
@@ -34,28 +33,28 @@ class InitialViewController: UIViewController, UITextFieldDelegate {
         loginButton.layer.borderWidth = 2.0
         loginButton.layer.borderColor = UIColor.white.cgColor
         loginButton.layer.cornerRadius = 7
-        loginButton.setTitle("Continue", for: .normal)
+        loginButton.setTitle("Login", for: .normal)
         loginButton.setTitleColor(.lightGray, for: .highlighted)
         loginButton.addTarget(self, action: #selector(loginButtonPressed), for: .touchUpInside)
         
-    
+        
         //emailTextField set up
         emailTextField.frame =  CGRect(x: CGFloat(0), y: loginButton.frame.minY - view.frame.midY, width: buttonWidth, height: CGFloat(buttonHeight) - 10.0)
         emailTextField.center.x = view.center.x
         emailTextField.textColor = .white
         emailTextField.returnKeyType = .continue
-        emailTextField.attributedPlaceholder = NSAttributedString(string: "Please Enter Your Email", attributes: [NSForegroundColorAttributeName: UIColor(red: 255.0/255.0, green: 255.0/255.0, blue: 255.0/255.0, alpha: 0.6)])
+        emailTextField.attributedPlaceholder = NSAttributedString(string: "Please Create A Password", attributes: [NSForegroundColorAttributeName: UIColor(red: 255.0/255.0, green: 255.0/255.0, blue: 255.0/255.0, alpha: 0.6)])
         emailTextField.delegate = self
         
         //email Label
         emailLabel.frame = emailTextField.frame
         emailLabel.center.y = emailTextField.center.y - emailTextField.bounds.height
         emailLabel.textColor = .white
-        emailLabel.text = "Email"
+        emailLabel.text = "Password"
         
         
         //welcome label
-        welcomeLabel.text = "Welcome to FitCat"
+        welcomeLabel.text = "Sign Up"
         welcomeLabel.frame = loginButton.frame
         welcomeLabel.center.y = (emailLabel.frame.minY)/2.0
         welcomeLabel.center.x = view.center.x
@@ -71,12 +70,12 @@ class InitialViewController: UIViewController, UITextFieldDelegate {
         //footer label
         footerLabel.frame = CGRect(x: emailTextField.frame.minX, y: emailTextField.frame.maxY + 10.0, width: buttonWidth, height: 20.0)
         footerLabel.center.y = lineBelowEmailTextField.frame.maxY + (footerLabel.frame.height/2.0) + 10.0
-        footerLabel.text = "Please Enter Your Email To Continue"
+        footerLabel.text = "Your Password Must Be 8 Characters Or Longer"
         footerLabel.textColor = .white
         footerLabel.adjustsFontSizeToFitWidth = true
         footerLabel.sizeToFit()
         
-   
+        
         view.addSubview(loginButton)
         view.addSubview(welcomeLabel)
         view.addSubview(emailTextField)
@@ -84,10 +83,10 @@ class InitialViewController: UIViewController, UITextFieldDelegate {
         view.layer.addSublayer(lineBelowEmailTextField)
         view.addSubview(footerLabel)
         view.addSubview(incorrectEmailFooterLabel)
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -98,11 +97,11 @@ class InitialViewController: UIViewController, UITextFieldDelegate {
     }
     
     func setUpGradient() {
-    let topColor = UIColor(red: 240.0/255.0, green: 97.0/255.0, blue: 68.0/255.0, alpha: 1.0).cgColor
-    let bottomColor = UIColor(red: 211.0/255.0, green: 61.0/255.0, blue: 43.0/255.0, alpha: 1.0).cgColor
-    gradient.colors = [topColor,bottomColor]
-    self.view.layer.insertSublayer(gradient, at: 0)
-}
+        let topColor = UIColor(red: 240.0/255.0, green: 97.0/255.0, blue: 68.0/255.0, alpha: 1.0).cgColor
+        let bottomColor = UIColor(red: 211.0/255.0, green: 61.0/255.0, blue: 43.0/255.0, alpha: 1.0).cgColor
+        gradient.colors = [topColor,bottomColor]
+        self.view.layer.insertSublayer(gradient, at: 0)
+    }
     func updateGradient() {
         gradient.frame = view.bounds
         gradient.locations = [0.0,1.0]
@@ -116,16 +115,14 @@ class InitialViewController: UIViewController, UITextFieldDelegate {
         emailTextField.resignFirstResponder()
         
         lineBelowEmailTextField.backgroundColor = emailTextField.text?.characters.count == 0 ? UIColor(red: 255.0/255.0, green: 255.0/255.0, blue: 255.0/255.0, alpha: 0.6).cgColor : UIColor.white.cgColor
-        isEmailValid = emailTextField.text?.characters.count == 0 ? false : isValidEmail(testStr: emailTextField.text!)
+        isEmailValid = emailTextField.text?.characters.count == 0 ? false : isValidPassword(testStr: emailTextField.text!)
         continueToNextScreen()
         return true
     }
     
     
-    func isValidEmail(testStr:String) -> Bool {
-        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
-        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
-        return emailTest.evaluate(with: testStr)
+    func isValidPassword(testStr:String) -> Bool {
+        return testStr.characters.count >= 8
     }
     
     func continueToNextScreen() {
@@ -133,32 +130,28 @@ class InitialViewController: UIViewController, UITextFieldDelegate {
             //display invalid email error
             footerLabel.isHidden = true
             incorrectEmailFooterLabel.frame = footerLabel.frame
-            incorrectEmailFooterLabel.text = "Oops. Please enter a valid email address."
+            incorrectEmailFooterLabel.text = "Oops. Please enter a valid password"
             incorrectEmailFooterLabel.textColor = UIColor(red: 217.0/255.0, green: 73.0/255.0, blue: 55.0/255.0, alpha: 1.0)
             incorrectEmailFooterLabel.backgroundColor = .white
             incorrectEmailFooterLabel.adjustsFontSizeToFitWidth = true
+            
             emailTextField.shake()
             
         } else {
             //check if email is in the system, if not show join page, if so show password screen
             footerLabel.isHidden = true
             incorrectEmailFooterLabel.isHidden = true
-            print("About to segue")
-            let signUpVC = SignUpViewController()
-            navigationController?.pushViewController(signUpVC, animated: true)
-            //self.present(signUpVC, animated: true, completion: nil)
-            print("pushed viewController")
         }
     }
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
